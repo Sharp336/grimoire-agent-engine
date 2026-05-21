tsagi2045/oh-my-pi

feat(coding-agent): add omp doctor + last-seen-version notice + dev-tree note
Seed seed_0ca7e1143ac1, Phase 3 + Phase 4 of the auto-update operating model.

- omp doctor: new top-level subcommand that reads
  ~/.omp/state/auto-update.json (written by the launchd auto-update job)
  and prints a Korean-first one-screen status with exactly one next
  action. Read-only; no git, no network.

- last-seen-version: one-line 'omp updated: A -> B' notice on the first
  new session after a real version change. Persisted at
  ~/.omp/state/last-seen-version; suppressed on first install.

- update-cli dev-tree note: when 'omp update' runs against a dev-tree
  relink the success path now explains the relink will restore the dev
  tree so the user does not read 'Updated to X.Y.Z' as 'new sessions
  will be on X.Y.Z'.

Refs: ~/.omp/specs/omp-update-model/seed.yaml

packages/coding-agent/src/cli/update-cli.ts
@@ -128,6 +128,79 @@ function resolveUpdateMethod(ompPath: string, bunBinDir: string | undefined): "b
 export function _resolveUpdateMethodForTest(ompPath: string, bunBinDir: string | undefined): "bun" | "binary" {
 	return resolveUpdateMethod(ompPath, bunBinDir);
 }
+
+/**
+ * Detect whether the resolved `omp` binary's symlink chain escapes the bun
+ * global install tree — i.e. the package directory has been redirected to a
+ * dev tree (typically by `bun link` or `~/.local/bin/omp-relink`).
+ *
+ * Returns the realpath of the resolved `omp` script when dev-tree mode is in
+ * effect, or `undefined` for the normal registry-installed case (and for any
+ * case where we can't determine the layout — we never assume dev-tree on
+ * uncertainty).
+ *
+ * We rely on the bun convention that the global install root sits at
+ * `<bunBinDir>/../install/global/node_modules`. For a registry install the
+ * realpath of `omp` lives somewhere under that root; for a dev-tree relink
+ * the package directory itself is a symlink that escapes the root.
+ */
+function detectDevTree(ompPath: string, bunBinDir: string | undefined): { realPath: string } | undefined {
+	if (!bunBinDir) return undefined;
+	const realOmp = tryRealpath(ompPath);
+	if (!realOmp) return undefined;
+	const installRoot = tryRealpath(path.resolve(bunBinDir, "..", "install", "global", "node_modules"));
+	if (!installRoot) return undefined;
+	if (isPathInDirectoryLexical(realOmp, installRoot)) return undefined;
+	return { realPath: realOmp };
+}
+
+export function _detectDevTreeForTest(
+	ompPath: string,
+	bunBinDir: string | undefined,
+): { realPath: string } | undefined {
+	return detectDevTree(ompPath, bunBinDir);
+}
+
+/**
+ * Print a multi-line note explaining that an `omp update` invocation is
+ * running against a dev-tree relink and therefore won't change what new
+ * sessions actually load.
+ */
+function printDevTreeNote(devTreeRealPath: string, latestVersion: string): void {
+	console.log(
+		chalk.yellow(
+			`Note: ${APP_NAME} resolves through a dev-tree symlink, not the npm package.`,
+		),
+	);
+	console.log(chalk.dim(`  Resolves to:        ${devTreeRealPath}`));
+	console.log(chalk.dim(`  Dev-tree version:   ${VERSION}`));
+	console.log(chalk.dim(`  Registry latest:    ${latestVersion}`));
+	console.log(
+		chalk.dim(
+			`  ${PACKAGE}@${latestVersion} will be installed into the bun global tree, but the`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  relink helper ($OMP_RELINK / ~/.local/bin/omp-relink) restores the dev tree`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  immediately afterwards. New sessions will keep loading the dev-tree version`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  (${VERSION}). To pick up the registry release, update the dev tree (e.g. git`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  pull / rebase) or remove the relink helper.`,
+		),
+	);
+}
 async function resolveUpdateTarget(): Promise<UpdateTarget> {
 	const bunBinDir = await getBunGlobalBinDir();
 	const ompPath = resolveOmpPath();
@@ -360,6 +433,22 @@ export async function runUpdateCommand(opts: { force: boolean; check: boolean })
 		console.log(chalk.yellow(`Forcing reinstall of ${release.version}`));
 	}
 
+	// Dev-tree detection: when the resolved `omp` realpath escapes the bun
+	// install root, the package directory has been redirected to a working
+	// tree. The install will still run, but the relink helper restores the
+	// dev tree afterwards — so the user's *next* session will keep loading
+	// the dev-tree version, not the registry release. We surface this up
+	// front so the install's success message isn't read as "new sessions
+	// will be on ${release.version}".
+	const ompPathForDetection = resolveOmpPath();
+	const bunBinDirForDetection = await getBunGlobalBinDir();
+	const devTree = ompPathForDetection
+		? detectDevTree(ompPathForDetection, bunBinDirForDetection)
+		: undefined;
+	if (devTree) {
+		printDevTreeNote(devTree.realPath, release.version);
+	}
+
 	if (opts.check) {
 		// Just check, don't install
 		return;
@@ -381,6 +470,16 @@ export async function runUpdateCommand(opts: { force: boolean; check: boolean })
 	// Dev-tree relink runs only after a successful install — no-op for
 	// end users who don't have an `omp-relink` helper on disk.
 	await runDevTreeRelinkIfPresent();
+
+	if (devTree) {
+		// Repeat the dev-tree summary at the end so it's the last thing the
+		// user sees, not buried above the install/relink output.
+		console.log(
+			chalk.yellow(
+				`Reminder: dev-tree relink restored ${devTree.realPath}; new sessions will continue to load ${VERSION} until the dev tree is updated.`,
+			),
+		);
+	}
 }
 
 /**

packages/coding-agent/test/update-cli.test.ts
@@ -1,5 +1,8 @@
-import { describe, expect, it } from "bun:test";
-import { _resolveUpdateMethodForTest } from "../src/cli/update-cli";
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import * as fs from "node:fs";
+import * as os from "node:os";
+import * as path from "node:path";
+import { _detectDevTreeForTest, _resolveUpdateMethodForTest } from "../src/cli/update-cli";
 
 describe("update-cli install target detection", () => {
 	it("uses bun update when prioritized omp is inside bun global bin", () => {
@@ -18,5 +21,92 @@ describe("update-cli install target detection", () => {
 		const method = _resolveUpdateMethodForTest("/Users/test/.local/bin/omp", undefined);
 
 		expect(method).toBe("binary");
+	});
+});
+
+describe("update-cli dev-tree detection", () => {
+	let tmpDir: string;
+
+	beforeEach(() => {
+		tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "omp-devtree-"));
+	});
+
+	afterEach(() => {
+		try {
+			fs.rmSync(tmpDir, { recursive: true, force: true });
+		} catch {
+			// best-effort cleanup
+		}
+	});
+
+	function makeBunBinDir(): string {
+		const bunBin = path.join(tmpDir, ".bun", "bin");
+		fs.mkdirSync(bunBin, { recursive: true });
+		return bunBin;
+	}
+
+	function makeRegistryInstall(bunBin: string): { ompShim: string; cli: string } {
+		// Layout mimics `bun install -g @oh-my-pi/pi-coding-agent`:
+		//   <tmp>/.bun/bin/omp -> ../install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts
+		//   <tmp>/.bun/install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts (real file)
+		const pkgDir = path.join(
+			tmpDir,
+			".bun",
+			"install",
+			"global",
+			"node_modules",
+			"@oh-my-pi",
+			"pi-coding-agent",
+		);
+		fs.mkdirSync(path.join(pkgDir, "src"), { recursive: true });
+		const cli = path.join(pkgDir, "src", "cli.ts");
+		fs.writeFileSync(cli, "// cli\n");
+		const ompShim = path.join(bunBin, "omp");
+		fs.symlinkSync(path.relative(bunBin, cli), ompShim);
+		return { ompShim, cli };
+	}
+
+	function makeDevTreeRelink(bunBin: string): { ompShim: string; devCli: string } {
+		// Layout mimics `omp-relink`:
+		//   <tmp>/.bun/install/global/node_modules/@oh-my-pi/pi-coding-agent -> <tmp>/dev/packages/coding-agent (symlink)
+		//   <tmp>/.bun/bin/omp -> ../install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts
+		const scope = path.join(tmpDir, ".bun", "install", "global", "node_modules", "@oh-my-pi");
+		const devPkg = path.join(tmpDir, "dev", "packages", "coding-agent");
+		fs.mkdirSync(scope, { recursive: true });
+		fs.mkdirSync(path.join(devPkg, "src"), { recursive: true });
+		const devCli = path.join(devPkg, "src", "cli.ts");
+		fs.writeFileSync(devCli, "// dev cli\n");
+		fs.symlinkSync(devPkg, path.join(scope, "pi-coding-agent"));
+		const ompShim = path.join(bunBin, "omp");
+		fs.symlinkSync(
+			path.relative(bunBin, path.join(scope, "pi-coding-agent", "src", "cli.ts")),
+			ompShim,
+		);
+		return { ompShim, devCli };
+	}
+
+	it("returns undefined when bun bin dir is unknown", () => {
+		const fakeOmp = path.join(tmpDir, "omp");
+		fs.writeFileSync(fakeOmp, "");
+		expect(_detectDevTreeForTest(fakeOmp, undefined)).toBeUndefined();
+	});
+
+	it("returns undefined when omp resolves inside the bun install tree", () => {
+		const bunBin = makeBunBinDir();
+		const { ompShim } = makeRegistryInstall(bunBin);
+		expect(_detectDevTreeForTest(ompShim, bunBin)).toBeUndefined();
+	});
+
+	it("returns the dev-tree realpath when the package directory is symlinked outside the bun install tree", () => {
+		const bunBin = makeBunBinDir();
+		const { ompShim, devCli } = makeDevTreeRelink(bunBin);
+		const result = _detectDevTreeForTest(ompShim, bunBin);
+		expect(result).toBeDefined();
+		expect(fs.realpathSync.native(result!.realPath)).toBe(fs.realpathSync.native(devCli));
+	});
+
+	it("returns undefined when the omp shim cannot be realpathed", () => {
+		const bunBin = makeBunBinDir();
+		expect(_detectDevTreeForTest(path.join(bunBin, "omp-missing"), bunBin)).toBeUndefined();
 	});
 });

Sharp336/grimoire-agent-engine

packages/coding-agent/src/cli/update-cli.ts
@@ -110,6 +110,79 @@ function resolveUpdateMethod(ompPath: string, bunBinDir: string | undefined): "b
 export function resolveUpdateMethodForTest(ompPath: string, bunBinDir: string | undefined): "bun" | "binary" {
 	return resolveUpdateMethod(ompPath, bunBinDir);
 }
+
+/**
+ * Detect whether the resolved `omp` binary's symlink chain escapes the bun
+ * global install tree — i.e. the package directory has been redirected to a
+ * dev tree (typically by `bun link` or `~/.local/bin/omp-relink`).
+ *
+ * Returns the realpath of the resolved `omp` script when dev-tree mode is in
+ * effect, or `undefined` for the normal registry-installed case (and for any
+ * case where we can't determine the layout — we never assume dev-tree on
+ * uncertainty).
+ *
+ * We rely on the bun convention that the global install root sits at
+ * `<bunBinDir>/../install/global/node_modules`. For a registry install the
+ * realpath of `omp` lives somewhere under that root; for a dev-tree relink
+ * the package directory itself is a symlink that escapes the root.
+ */
+function detectDevTree(ompPath: string, bunBinDir: string | undefined): { realPath: string } | undefined {
+	if (!bunBinDir) return undefined;
+	const realOmp = tryRealpath(ompPath);
+	if (!realOmp) return undefined;
+	const installRoot = tryRealpath(path.resolve(bunBinDir, "..", "install", "global", "node_modules"));
+	if (!installRoot) return undefined;
+	if (isPathInDirectoryLexical(realOmp, installRoot)) return undefined;
+	return { realPath: realOmp };
+}
+
+export function detectDevTreeForTest(
+	ompPath: string,
+	bunBinDir: string | undefined,
+): { realPath: string } | undefined {
+	return detectDevTree(ompPath, bunBinDir);
+}
+
+/**
+ * Print a multi-line note explaining that an `omp update` invocation is
+ * running against a dev-tree relink and therefore won't change what new
+ * sessions actually load.
+ */
+function printDevTreeNote(devTreeRealPath: string, latestVersion: string): void {
+	console.log(
+		chalk.yellow(
+			`Note: ${APP_NAME} resolves through a dev-tree symlink, not the npm package.`,
+		),
+	);
+	console.log(chalk.dim(`  Resolves to:        ${devTreeRealPath}`));
+	console.log(chalk.dim(`  Dev-tree version:   ${VERSION}`));
+	console.log(chalk.dim(`  Registry latest:    ${latestVersion}`));
+	console.log(
+		chalk.dim(
+			`  ${PACKAGE}@${latestVersion} will be installed into the bun global tree, but the`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  relink helper ($OMP_RELINK / ~/.local/bin/omp-relink) restores the dev tree`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  immediately afterwards. New sessions will keep loading the dev-tree version`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  (${VERSION}). To pick up the registry release, update the dev tree (e.g. git`,
+		),
+	);
+	console.log(
+		chalk.dim(
+			`  pull / rebase) or remove the relink helper.`,
+		),
+	);
+}
 async function resolveUpdateTarget(): Promise<UpdateTarget> {
 	const bunBinDir = await getBunGlobalBinDir();
 	const ompPath = resolveOmpPath();
@@ -364,6 +437,22 @@ export async function runUpdateCommand(opts: { force: boolean; check: boolean })
 		console.log(chalk.yellow(`Forcing reinstall of ${release.version}`));
 	}
 
+	// Dev-tree detection: when the resolved `omp` realpath escapes the bun
+	// install root, the package directory has been redirected to a working
+	// tree. The install will still run, but the relink helper restores the
+	// dev tree afterwards — so the user's *next* session will keep loading
+	// the dev-tree version, not the registry release. We surface this up
+	// front so the install's success message isn't read as "new sessions
+	// will be on ${release.version}".
+	const ompPathForDetection = resolveOmpPath();
+	const bunBinDirForDetection = await getBunGlobalBinDir();
+	const devTree = ompPathForDetection
+		? detectDevTree(ompPathForDetection, bunBinDirForDetection)
+		: undefined;
+	if (devTree) {
+		printDevTreeNote(devTree.realPath, release.version);
+	}
+
 	if (opts.check) {
 		// Just check, don't install
 		return;
@@ -380,6 +469,20 @@ export async function runUpdateCommand(opts: { force: boolean; check: boolean })
 	} catch (err) {
 		console.error(chalk.red(`Update failed: ${err}`));
 		process.exit(1);
+	}
+
+	// Dev-tree relink runs only after a successful install — no-op for
+	// end users who don't have an `omp-relink` helper on disk.
+	await runDevTreeRelinkIfPresent();
+
+	if (devTree) {
+		// Repeat the dev-tree summary at the end so it's the last thing the
+		// user sees, not buried above the install/relink output.
+		console.log(
+			chalk.yellow(
+				`Reminder: dev-tree relink restored ${devTree.realPath}; new sessions will continue to load ${VERSION} until the dev tree is updated.`,
+			),
+		);
 	}
 }
 

packages/coding-agent/test/update-cli.test.ts
@@ -1,8 +1,13 @@
-import { afterEach, describe, expect, it } from "bun:test";
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
 import * as fs from "node:fs/promises";
+import * as fsSync from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
-import { replaceBinaryForUpdate, resolveUpdateMethodForTest } from "../src/cli/update-cli";
+import {
+	detectDevTreeForTest,
+	replaceBinaryForUpdate,
+	resolveUpdateMethodForTest,
+} from "../src/cli/update-cli";
 
 const tempDirs: string[] = [];
 
@@ -78,5 +83,92 @@ describe("update-cli binary replacement", () => {
 		expect(await Bun.file(targetPath).text()).toBe("new binary");
 		expect(await Bun.file(tempPath).exists()).toBe(false);
 		expect(await Bun.file(backupPath).exists()).toBe(false);
+	});
+});
+
+describe("update-cli dev-tree detection", () => {
+	let tmpDir: string;
+
+	beforeEach(() => {
+		tmpDir = fsSync.mkdtempSync(path.join(os.tmpdir(), "omp-devtree-"));
+	});
+
+	afterEach(() => {
+		try {
+			fsSync.rmSync(tmpDir, { recursive: true, force: true });
+		} catch {
+			// best-effort cleanup
+		}
+	});
+
+	function makeBunBinDir(): string {
+		const bunBin = path.join(tmpDir, ".bun", "bin");
+		fsSync.mkdirSync(bunBin, { recursive: true });
+		return bunBin;
+	}
+
+	function makeRegistryInstall(bunBin: string): { ompShim: string; cli: string } {
+		// Layout mimics `bun install -g @oh-my-pi/pi-coding-agent`:
+		//   <tmp>/.bun/bin/omp -> ../install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts
+		//   <tmp>/.bun/install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts (real file)
+		const pkgDir = path.join(
+			tmpDir,
+			".bun",
+			"install",
+			"global",
+			"node_modules",
+			"@oh-my-pi",
+			"pi-coding-agent",
+		);
+		fsSync.mkdirSync(path.join(pkgDir, "src"), { recursive: true });
+		const cli = path.join(pkgDir, "src", "cli.ts");
+		fsSync.writeFileSync(cli, "// cli\n");
+		const ompShim = path.join(bunBin, "omp");
+		fsSync.symlinkSync(path.relative(bunBin, cli), ompShim);
+		return { ompShim, cli };
+	}
+
+	function makeDevTreeRelink(bunBin: string): { ompShim: string; devCli: string } {
+		// Layout mimics `omp-relink`:
+		//   <tmp>/.bun/install/global/node_modules/@oh-my-pi/pi-coding-agent -> <tmp>/dev/packages/coding-agent (symlink)
+		//   <tmp>/.bun/bin/omp -> ../install/global/node_modules/@oh-my-pi/pi-coding-agent/src/cli.ts
+		const scope = path.join(tmpDir, ".bun", "install", "global", "node_modules", "@oh-my-pi");
+		const devPkg = path.join(tmpDir, "dev", "packages", "coding-agent");
+		fsSync.mkdirSync(scope, { recursive: true });
+		fsSync.mkdirSync(path.join(devPkg, "src"), { recursive: true });
+		const devCli = path.join(devPkg, "src", "cli.ts");
+		fsSync.writeFileSync(devCli, "// dev cli\n");
+		fsSync.symlinkSync(devPkg, path.join(scope, "pi-coding-agent"));
+		const ompShim = path.join(bunBin, "omp");
+		fsSync.symlinkSync(
+			path.relative(bunBin, path.join(scope, "pi-coding-agent", "src", "cli.ts")),
+			ompShim,
+		);
+		return { ompShim, devCli };
+	}
+
+	it("returns undefined when bun bin dir is unknown", () => {
+		const fakeOmp = path.join(tmpDir, "omp");
+		fsSync.writeFileSync(fakeOmp, "");
+		expect(detectDevTreeForTest(fakeOmp, undefined)).toBeUndefined();
+	});
+
+	it("returns undefined when omp resolves inside the bun install tree", () => {
+		const bunBin = makeBunBinDir();
+		const { ompShim } = makeRegistryInstall(bunBin);
+		expect(detectDevTreeForTest(ompShim, bunBin)).toBeUndefined();
+	});
+
+	it("returns the dev-tree realpath when the package directory is symlinked outside the bun install tree", () => {
+		const bunBin = makeBunBinDir();
+		const { ompShim, devCli } = makeDevTreeRelink(bunBin);
+		const result = detectDevTreeForTest(ompShim, bunBin);
+		expect(result).toBeDefined();
+		expect(fsSync.realpathSync.native(result!.realPath)).toBe(fsSync.realpathSync.native(devCli));
+	});
+
+	it("returns undefined when the omp shim cannot be realpathed", () => {
+		const bunBin = makeBunBinDir();
+		expect(detectDevTreeForTest(path.join(bunBin, "omp-missing"), bunBin)).toBeUndefined();
 	});
 });