525f3e6eddeb3fa77d58ab181adbb7ef090b597c
Admin <zengwangdong88@foxmail.com>
2026-08-14 16:09:50 +0800

    fixed: https://github.com/can1357/oh-my-pi/issues/4201, support 【Volcengine Agent Plan】 & 【volcengine-coding-plan】
    Signed-off-by: Admin <zengwangdong88@foxmail.com>

--- packages/ai/src/registry/registry.ts
+++ packages/ai/src/registry/registry.ts
@@ -75,6 +75,8 @@ import { xiaomiTokenPlanSgpProvider } from "./xiaomi-token-plan-sgp";
 import { zaiCodingPlanProvider, zaiProvider } from "./zai";
 import { zenmuxProvider } from "./zenmux";
 import { zhipuCodingPlanProvider } from "./zhipu-coding-plan";
+import {volcengineCodingPlanProvider} from "./volcengine-coding-plan";
+import {volcengineAgentPlanProvider} from "./volcengine-agent-plan";
 
 /**
  * The single per-provider list. Adding a provider = create `./providers/<id>.ts`
@@ -160,6 +162,8 @@ const ALL = [
 	amazonBedrockProvider,
 	bedrockMantleProvider,
 	gmiCloudProvider,
+    volcengineAgentPlanProvider,
+    volcengineCodingPlanProvider,
 ];
 
 export type RegistryDef = (typeof ALL)[number];
